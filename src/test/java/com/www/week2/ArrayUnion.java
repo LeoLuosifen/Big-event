aa84cbf2ff59532fcc5cc0c1161d0d672d6fb996
LeoLuosifen <www.luoclass@gmail.com>
2024-02-27 15:06:12 +1100

    Correct

--- src/test/java/com/www/week2/ArrayUnion.java
+++ src/test/java/com/www/week2/ArrayUnion.java
@@ -13,10 +13,6 @@ public class ArrayUnion {
             return null;
         }
 
-        if (a.length == 0 || b.length == 0){
-            return null;
-        }
-
 
             if (flag){
                 int[] arr = new int[resultArr.length];
